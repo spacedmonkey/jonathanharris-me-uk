a60cbca7acc3431a3f287b0d15ea879053cd6294
Jonny Harris <jonathan.harris@skyscanner.net>
2019-04-02 14:23:40 +0100

    Add function.php file

--- functions.php
+++ functions.php
@@ -534,3 +534,20 @@ function spacedmonkey_contains($str, array $arr)
     }
     return false;
 }
+
+
+if ( ! function_exists( 'wp_body_open' ) ) {
+    /**
+     * Fire the wp_body_open action.
+     *
+     * Added for backwards compatibility to support WordPress versions prior to 5.2.0.
+     *
+     */
+    function wp_body_open() {
+        /**
+         * Triggered after the opening <body> tag.
+         *
+         */
+        do_action( 'wp_body_open' );
+    }
+}
